676924bde11f84d21b7b84d7fe780979d37d0a04
Jason McNair <jason.mcnair@oliverwyman.com>
2024-11-09 13:43:57 -0500

    port forwarding for AO mongo databases

---
+++ Bash/ao-forward-ports.sh
@@ -0,0 +1,5 @@
+kubectl -n assortment-production-adfl port-forward ao-mongodb-0 60001:27017 &
+kubectl -n assortment-production-adgc port-forward ao-mongodb-0 60002:27017 &
+kubectl -n assortment-production-adgf port-forward ao-mongodb-0 60003:27017 &
+kubectl -n assortment-production-adhf port-forward ao-mongodb-0 60004:27017 &
+kubectl -n assortment-production-adss port-forward ao-mongodb-0 60005:27017 &
